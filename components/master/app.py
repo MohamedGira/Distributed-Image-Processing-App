96188889655a4a0c7cee5b8db04753007eac283d
Mohamed Gira <mohamedgira0901@gmail.com>
2024-05-02 06:26:39 +0300

    old master stuff

--- components/master/app.py
+++ components/master/app.py
@@ -9,7 +9,11 @@ from utils.Submitter import rabbitmq_submitter
 from utils.Database.database import RedisDatabase
 from dotenv import load_dotenv
 from io import BytesIO
+from events import socketio
 
+
+app = Flask(__name__)
+socketio.init_app(app)
 ##aws client config
 
 load_dotenv()
@@ -19,75 +23,64 @@ storage=disk_storage.DiskStorage(os.getenv("SHARED_STORAGE_PATH"))
 submitter=rabbitmq_submitter.RabbitMQSubmitter(os.getenv("RABBITMQ_HOST"),os.getenv("RABBITMQ_QUEUE_NAME"),os.getenv("RABBITMQ_PORT"))
 database=RedisDatabase(os.getenv("REDIS_HOST"),os.getenv("REDIS_PORT"))
 
-app = Flask(__name__)
+
 CORS(app, origins=[""])  #TODO: set to desired later
 
-@app.route("/upload", methods=["POST","GET"])
+@app.route("/upload", methods=["GET"])
 def upload():
-    if request.method == "GET":
-        return render_template('index.html')
+    return render_template('index.html')
 
-    if "images" not in request.files or "process" not in request.form:
+@app.route("/uploadOne", methods=["POST"])
+def uploadOne():
+    if "image" not in request.files or "process" not in request.form:
         return f"Missing images or text field,{request.form} ", 400
-    # Get the list of images from the POST request
-    images = request.files.getlist("images")
-    print(request.files)
-    print(request.files.getlist("images"))
-    # Get the text from the POST request
+    #Get the list of images from the POST request
+    image = request.files.get("image")
     operation = request.form["process"]
-    task_ids = []
-    queue_requests = []
-    for image in images:
-        task_id = str(uuid.uuid4())
-        extension=image.content_type.split('/')[1] if len(image.content_type.split('/'))>1 else "png"
-        # upload image to s3
-        stream=image.read()
-        response = storage.save(BytesIO(stream), f"{task_id}.{extension}", path="inputs")
-        if response is None:
-            task_ids.append(00)
-        else:
-            queue_request = {
-                "task_id": task_id,
-                "operation": operation,
-                "access_means": response,
-                "status": "pending",
-                "extension":extension,
-                "last_updated": f"{datetime.now()}",
-            }
-            queue_requests.append(queue_request)
-            task_ids.append(task_id)
-    #option, submit immediatly
-    for queue_request in queue_requests:
-        database.save_dict(task_id,queue_request)
-        submitter.submit(queue_request)
-        
-
-    return "Images and text received successfully", 200
+    task_id = str(uuid.uuid4())
+    extension=image.content_type.split('/')[1] if len(image.content_type.split('/'))>1 else "png"
+    # upload image to s3
+    stream=image.read()
+    #response = storage.save(BytesIO(stream), f"{task_id}.{extension}", path="inputs")
+    response = "mock"
+    if response is None:
+        return {"message":"Error uploading image"}, 400
+    else:
+        queue_request = {
+            "task_id": task_id,
+            "operation": operation,
+            "access_means": response,
+            "status": "pending",
+            "extension":extension,
+            "last_updated": f"{datetime.now()}",
+        }
+        return queue_request, 200
 
 #TODO: for s3, check how image must be returned. loaded then sent?
 def retrieve_image(output_means):
     if issubclass(storage,disk_storage.DiskStorage):
         return output_means
-    elif issubclass(storage,disk_storage.DiskStorage):
+    elif issubclass(storage,s3_storage.S3StorageManager):
         return output_means
     else:
         raise NotImplementedError("shouldn't reach here")
+    
 #TODO: 0 auth, 0 denial of service protection... but meh
-@app.route("/status/<task_id>", methods=["GET"])
-def status(task_id):
-    if task_id is None:
-        return "Task ID not provided", 400
-    response=database.get_dict(task_id)
-    print(response.get("status"))
-    if response.get("status") != "success":
-        return {"status":response.get("status")}, 200
-    else:
-        return send_file(response.get("output_means"))
-    # Get the status of the task from the database
-    return status, 200
+@app.route("/result",methods=["GET"])
+def result():
+    output_means=request.args.get("path")
+    if output_means is None:
+        return "Missing path", 400
+    try:
+        return send_file(output_means)
+    except Exception as e:
+        return str(e), 400
+
+
+
 
 if __name__ == "__main__":
-    app.run(port=8000,debug=True)
+    socketio.run(app,port=8000,debug=True)
 
     
 
